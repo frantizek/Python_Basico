5d332a1cc79d4aabc343b5c4b5f50e1332f52cca
frantizek <32653773+frantizek@users.noreply.github.com>
2024-06-29 22:09:28 -0600

    first batch of changes
    from day 11 to day 15

--- codecademy/create_py_files.py
+++ codecademy/create_py_files.py
@@ -5,23 +5,29 @@ import sys
 
 # def main
 def main():
-    for practice in range(2, 16):
-        print(f"day{str(practice).zfill(2)}")
-        ifExist = os.path.exists(f"day{str(practice).zfill(2)}")
-        if not ifExist:
-            # Create a new directory because it does not exist
-            os.makedirs(f"day{str(practice).zfill(2)}")
-            print("The new directory is created!")
-        if ifExist:
-            # Create a new directory because it does not exist
-            os.chdir(f"day{str(practice).zfill(2)}")
-            print("cd into directory!")
-            # os.makedirs("practice_questions")
-            os.chdir("practice_questions")
-            for practice_num in range(1, 11):
-                with open(f"practice_{str(practice_num).zfill(2)}.py", 'w') as document:
-                    pass
-            os.chdir("../..")
+    pass
+    # for practice in range(5, 16):
+    #     print(f"day{str(practice).zfill(2)}")
+    #     ifExist = os.path.exists(f"day{str(practice).zfill(2)}")
+    #     if not ifExist:
+    #         # Create a new directory because it does not exist
+    #         os.makedirs(f"day{str(practice).zfill(2)}")
+    #         print("The new directory is created!")
+    #     if ifExist:
+    #         # Create a new directory because it does not exist
+    #         os.chdir(f"day{str(practice).zfill(2)}")
+    #         print(f"cd into directory day{str(practice).zfill(2)}!")
+    #
+    #         for practice_num in range(1, 4):
+    #             with open(f"day{str(practice).zfill(2)}_exercise{str(practice_num).zfill(2)}.py", 'w') as document:
+    #                 pass
+    #
+    #         # os.makedirs("practice_questions")
+    #         os.chdir("practice_questions")
+    #         # for practice_num in range(1, 11):
+    #         #     with open(f"practice_{str(practice_num).zfill(2)}.py", 'w') as document:
+    #         #         pass
+    #         os.chdir("../..")
 
 
 # main
